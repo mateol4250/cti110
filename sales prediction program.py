f3fdb333426f9d3ba8c7d8d34bd2380a9c564633
mateol4250 <32048090+mateol4250@users.noreply.github.com>
2017-09-18 13:27:16 -0400

    Add files via upload

---
+++ sales prediction program.py
@@ -0,0 +1,11 @@
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Luis Mateo
+# 9-18-17
+#This program will calculate profit
+
+totalSales= float(input ( "please enter total sales: "))
+
+profit= totalSales * .23
+
+print("profit= ", profit) 
